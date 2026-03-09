90f7269f4fae5df4a783161be67eb2e7a5c027e5
Brayemar <mogikoyob@gmail.com>
2026-03-09 08:09:33 +0300

    updates

--- src/components/Sixthcomponent.jsx
+++ src/components/Sixthcomponent.jsx
@@ -1,11 +1,12 @@
 import {useState} from 'react';
 const Sixthcomponent = () =>{
     const [weight, setWeight] = useState("50kg")
+    const [message, setMessage] = useState("The new weight is: ")
     return(
         <div className="Sixthcomponent">
             <h1>This is the Sixth component</h1>
             <h2>The weight is:{weight}</h2>
-            <button onClick={ () => setWeight("55kg")}>Click to update the Number</button>
+            <button onClick={ () => [setWeight("55kg"), setMessage("The new weight is: ")]}>Click to update the Number</button>
         </div>
     )
 }
